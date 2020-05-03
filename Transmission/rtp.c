98b61fe2cd9d804f07f947c149b3ccb2a2f6ec3e
xue0708 <1735590087@qq.com>
2020-05-03 18:04:49 +0800

    add

---
+++ Transmission/rtp.c
@@ -0,0 +1,44 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "rtp.h"
+
+void rtpHeaderInit(struct RtpPacket* rtpPacket, uint8_t csrcLen, uint8_t extension,
+                    uint8_t padding, uint8_t version, uint8_t payloadType, uint8_t marker,
+                   uint16_t seq, uint32_t timestamp, uint32_t ssrc)
+{
+    rtpPacket->rtpHeader.csrcLen = csrcLen;
+    rtpPacket->rtpHeader.extension = extension;
+    rtpPacket->rtpHeader.padding = padding;
+    rtpPacket->rtpHeader.version = version;
+    rtpPacket->rtpHeader.payloadType =  payloadType;
+    rtpPacket->rtpHeader.marker = marker;
+    rtpPacket->rtpHeader.seq = seq;
+    rtpPacket->rtpHeader.timestamp = timestamp;
+    rtpPacket->rtpHeader.ssrc = ssrc;
+}
+
+int rtpSendPacket(int socket, const char* ip, int16_t port, struct RtpPacket* rtpPacket, uint32_t dataSize)
+{
+    struct sockaddr_in addr;
+    int ret;
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip);
+
+    rtpPacket->rtpHeader.seq = htons(rtpPacket->rtpHeader.seq);
+    rtpPacket->rtpHeader.timestamp = htonl(rtpPacket->rtpHeader.timestamp);
+    rtpPacket->rtpHeader.ssrc = htonl(rtpPacket->rtpHeader.ssrc);
+
+    ret = sendto(socket, (void*)rtpPacket, dataSize+RTP_HEADER_SIZE, 0,
+                    (struct sockaddr*)&addr, sizeof(addr));
+
+    rtpPacket->rtpHeader.seq = ntohs(rtpPacket->rtpHeader.seq);
+    rtpPacket->rtpHeader.timestamp = ntohl(rtpPacket->rtpHeader.timestamp);
+    rtpPacket->rtpHeader.ssrc = ntohl(rtpPacket->rtpHeader.ssrc);
+
+    return ret;
+}
